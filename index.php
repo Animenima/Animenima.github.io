c296dc917b468aef2e9c59b18fa3fc6efb7be685
Animenima <animenima@gmail.com>
2022-03-31 19:53:23 -0400

    changes

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type='text/css'>
+		body {
+			background-image:url(https://raw.githubusercontent.com/Animenima/Animenima.github.io/main/background.png);
+			background-color: gray;
+		}
+		div#one {
+			z-index: 1000;
+		}
+	</style>
+</head>
+<body>
+	<h1 style="color:orange;
+						 text-align:center;
+						 font-size:60px;">
+						 Welcome to Animenima
+	</h1>
+	<p style="color:red;
+	          text-align:center;
+						font-size:30px;">
+						Continue
+	</p>
+<div id="one"
+     style="color:yellow;
+		 text-align:center;"
+	<a href="page2.html">
+		Main Page
+	</a>
+</div>
+</body>
+</html>
